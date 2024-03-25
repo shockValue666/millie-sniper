01d52ca166284962739bfe7d4506005e1cc0f63a
shockValue666 <mapedeveloper@protonmail.com>
2024-03-24 20:21:45 -0700

    fourth

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,13 @@ import { v4 } from 'uuid';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
-
+import { createClient } from '@supabase/supabase-js';
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, sendAndConfirmTransaction } from '@solana/web3.js';
+import base58 from 'bs58';
+import { checkTransaction, saveTransaction, sendFee, updateBalance } from '../helpers';
+const supabaseUrl = process.env.SUPABASE_URL || "";
+const supabaseKey = process.env.SUPABASE_KEY || "";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 
