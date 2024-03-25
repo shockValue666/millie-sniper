99f33aa18dfe38749b450451e422a5a9bec22f72
shockValue666 <mapedeveloper@protonmail.com>
2024-03-24 20:13:41 -0700

    first

--- api/index.ts
+++ api/index.ts
@@ -62,63 +62,63 @@ app.get("/", (request: Request, response: Response) => {
     response.status(200).send("Server is running");
 });
 
-app.post('/webhook', (request: Request, response: Response) => {
-    const requestBody = request.body;   
-    //print the json to the console
-    console.log("data received by the webhook: ", requestBody);
+// app.post('/webhook', (request: Request, response: Response) => {
+//     const requestBody = request.body;   
+//     //print the json to the console
+//     console.log("data received by the webhook: ", requestBody);
 
-    //send a response that we received and processed the request
-    response.status(200).send('Webhook received the request lolllll');
-});
+//     //send a response that we received and processed the request
+//     response.status(200).send('Webhook received the request lolllll');
+// });
 
-app.post("/",async (request: Request, response: Response) => {
-    console.log("root")
-    console.log("request body: ",typeof request.body);
-    const newId = v4()
-    const newDate = new Date().toISOString();
-    console.log("newId: ",newId, " newDate: ",newDate);
-    const data = await fetchSampleData();
-    console.log("data from the post: ",data);
-    console.log("after")
+// app.post("/",async (request: Request, response: Response) => {
+//     console.log("root")
+//     console.log("request body: ",typeof request.body);
+//     const newId = v4()
+//     const newDate = new Date().toISOString();
+//     console.log("newId: ",newId, " newDate: ",newDate);
+//     const data = await fetchSampleData();
+//     console.log("data from the post: ",data);
+//     console.log("after")
 
-    response.status(200).send("Request received");
-})
-
-
-app.post("/test",async (request: Request, response: Response) => {
-    // console.log("test",JSON.stringify(request.body))
-    const data = await addTransactionHash(JSON.stringify(request.body));
-    if(data){
-        console.log("data from the post: ",data);
-        return response.status(200).send("Request received");
-    }
-    return response.status(500).send("Error adding transaction hash");
-})
+//     response.status(200).send("Request received");
+// })
 
 
-app.post('/details', async (request: Request, response: Response) => {
-    const requestBody = request.body;   
-    const amount = requestBody[0].nativeTransfers[0].amount/1000000000;
-    if(request.body[0]){
-        const transactionType = await checkTransaction(request.body);
-        const {status} = transactionType || {status:""};
-        if(status==="big boss"){
-            console.log("BIG BOSS FEE!!!")
-            return;
-        }
-        const savedStatus = await saveTransaction(request.body);
-        await updateBalance(request.body,transactionType || {status:""});
-        await sendFee(request.body,transactionType || {status:""});
-    }
-    //print the json to the console
+// app.post("/test",async (request: Request, response: Response) => {
+//     // console.log("test",JSON.stringify(request.body))
+//     const data = await addTransactionHash(JSON.stringify(request.body));
+//     if(data){
+//         console.log("data from the post: ",data);
+//         return response.status(200).send("Request received");
+//     }
+//     return response.status(500).send("Error adding transaction hash");
+// })
 
-    //send a response that we received and processed the request
-    response.status(200).send('Webhook received the request funny (straight face)');
-});
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+// app.post('/details', async (request: Request, response: Response) => {
+//     const requestBody = request.body;   
+//     const amount = requestBody[0].nativeTransfers[0].amount/1000000000;
+//     if(request.body[0]){
+//         const transactionType = await checkTransaction(request.body);
+//         const {status} = transactionType || {status:""};
+//         if(status==="big boss"){
+//             console.log("BIG BOSS FEE!!!")
+//             return;
+//         }
+//         const savedStatus = await saveTransaction(request.body);
+//         await updateBalance(request.body,transactionType || {status:""});
+//         await sendFee(request.body,transactionType || {status:""});
+//     }
+//     //print the json to the console
+
+//     //send a response that we received and processed the request
+//     response.status(200).send('Webhook received the request funny (straight face)');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server is running at http://localhost:${port}`);
+// });
 
 
 
